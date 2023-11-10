48f6ad16688c242d25e5d66b969c1f29d93e6257
Elliott Hager <83028849+eHag-FRU@users.noreply.github.com>
2023-11-09 22:51:39 -0500

    Added in JavaScript Refresher

---
+++ Solutions/Day 0/index.js
@@ -0,0 +1,20 @@
+//1 & 3
+const Dog = {
+
+    legs : 4,
+    color: 'brown',
+    age: 2,
+
+    bark: function() {
+        console.log('Woof woof')
+    }
+}
+
+//2
+console.log(Object.values(Dog))
+console.log(Dog)
+
+
+//4
+
+
