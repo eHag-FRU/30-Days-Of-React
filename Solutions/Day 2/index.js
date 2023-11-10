065441c9c80eb8a29dc88cf141d8f6f386796288
Elliott Hager <83028849+eHag-FRU@users.noreply.github.com>
2023-11-10 00:00:40 -0500

    Finished Day 2!!

---
+++ Solutions/Day 2/index.js
@@ -0,0 +1,16 @@
+const jsxElement = <h1>I am a JSX element</h1>  //JS with HTML
+const title = <h2>Getting Started React</h2>
+
+const head = (
+    <header>
+        <h1>Welcome to 30 Days of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>JavaScript Library</h3>
+        <p>Elliott Hager</p>
+        <small>November 9, 2023</small>
+    </header>
+)
+
+
+
+
