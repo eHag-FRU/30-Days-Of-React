09b3ee6adda2804a1e47dfdb4d88229087679adf
Elliott Hager <83028849+eHag-FRU@users.noreply.github.com>
2023-11-10 12:30:42 -0500

    Finished the basics of Day 03

--- Solutions/Day 3/example-react-app/src/index.js
+++ Solutions/Day 3/example-react-app/src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const main = (
+  <main>
+    <p>Prequisite to get started react.js</p>  
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+const header = (
+    <header>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 2, 2020</small> 
+    </header>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+
+
+
+)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(app, rootElement)
