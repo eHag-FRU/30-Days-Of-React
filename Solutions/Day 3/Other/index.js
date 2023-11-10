104b8c2c97039b1d8ac3f508d2593da720d139fd
Elliott Hager <83028849+eHag-FRU@users.noreply.github.com>
2023-11-10 11:49:16 -0500

    Created First React App

---
+++ Solutions/Day 3/Other/index.js
@@ -0,0 +1,9 @@
+import doSomeMath from './math.js'
+
+import { addTwo, multiply, subtract } from "./math"
+
+import * as everything from './math.js'
+
+console.log(addTwo(5,5))
+console.log(doSomeMath.addTwo(5,5))
+console.log(everything)
